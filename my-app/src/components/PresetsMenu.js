898bbf6375a37374123a6dbf04e6d9e3718020c3
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-03-04 14:22:31 -0800

    feat: presets menu

--- my-app/src/components/PresetsMenu.js
+++ my-app/src/components/PresetsMenu.js
@@ -1,8 +1,18 @@
-function PresetsMenu(props) {
+import './PresetsMenu.css'
 
+function PresetsMenu(props) {
     return (
         <div className="presets-menu">
             <h2>Presets</h2>
+            {/* multiple choice (choose one)*/}
+            <ul className="preset-list">
+                {props.presets.map((preset) => (
+                    <li key={preset} className="preset-list-item">
+                        <label htmlFor={preset}>{preset}</label>
+                    </li>
+                ))}
+            </ul>
+
         </div>
     )
 }
