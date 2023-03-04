d899c8076734b3b87eb0eef1ca3a3333bde82b7d
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-03-04 14:18:00 -0800

    feat: results page only loads when information is complete

--- my-app/src/components/pages/Results.js
+++ my-app/src/components/pages/Results.js
@@ -1,9 +1,16 @@
 import './Page.css';
 
-function Results() { 
+function Results(props) { 
+    if (!props.informationComplete) {
+        return (
+            <div className="page">
+                <h1>Oops! Please go back and make sure that you filled out all the information.</h1>
+            </div>
+        )
+    }
     return (
         <div className="page">
-            <h1>Results</h1>
+            <h1>Enjoy your workout!</h1>
         </div>
     )
 }
