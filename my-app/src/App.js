3b887b50e474cca30072e7da4033948554c61901
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-03-04 16:01:39 -0800

    feat: add menu for specific exercises

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,16 @@ import MuscleGroups from './components/pages/MuscleGroups';
 import SelectExercisesAndCalculate from './components/pages/SelectExercisesAndCalculate.js';
 import Results from './components/pages/Results';
 
+// temp value, to get from backend later
+const exerciseCount = 3;
+
 function App() {
   return (
     <div className="App">
       <div id="page-container">
         <StartingInfo/>
         <MuscleGroups/>
-        <SelectExercisesAndCalculate/>
+        <SelectExercisesAndCalculate exercises={["pull-ups", "sit-ups", "deadlift", "squats"]} exerciseCount={exerciseCount}/>
         <Results/>
       </div>
     </div>
--- my-app/src/components/pages/SelectExercisesAndCalculate.js
+++ my-app/src/components/pages/SelectExercisesAndCalculate.js
@@ -1,9 +1,41 @@
 import './Page.css';
+import './SelectExercisesAndCalculate.css'
 
-function SelectExercisesAndCalculate() {
+function SelectExercisesAndCalculate(props) {
+
+    function exercisePrompts(exerciseCount, exercises) {
+        // return an array of exercise prompts length equal to exerciseCount
+        let prompts = [];
+        for (let i = 0; i < exerciseCount; i++) {
+            prompts.push(
+                <div className="exercise-prompt">
+                    <p>Exercise {i+1}</p>
+                    <select>
+                        {exercises.map((exercise) => {
+                            return <option value={exercise}>{exercise}</option>
+                        })}
+                    </select>
+                    <input type="number" placeholder="Warmup Sets"></input>
+                    <input type="number" placeholder="Work Set Weight"></input>
+                </div>
+            )
+        }
+        return prompts;
+    }
+
     return (
-        <div className="page">
-            <h1>One last step...</h1>
+        <div className="page third-page">
+            <h1>You're almost there...</h1>
+            {/* list of exercises to choose, select number of warmup sets (default 3), input work set weight */}
+            
+            {exercisePrompts(props.exerciseCount, props.exercises)}
+
+            {/* input asking for number of warmup sets */}
+            {/* input asking for work set weight */}
+
+
+            {/* submit button */}
+
         </div>
     )
 }
